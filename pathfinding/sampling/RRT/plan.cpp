0a86d9ee601c641874b6f8cd56899ac0f640431b
Naum Azeredo <naumazeredo@gmail.com>
2014-05-25 17:43:03 -0300

    Base plan for all RRT algorithms

--- pathfinding/sampling/RRT/plan.cpp
+++ pathfinding/sampling/RRT/plan.cpp
@@ -1,8 +1,15 @@
-#include <cassert>
+#include <iostream>
 
-#include <boost/geometry/algorithms/comparable_distance.hpp>
-#include <boost/numeric/conversion/bounds.hpp>
-#include <boost/geometry/algorithms/intersects.hpp>
+#include <ctime>
+#include <limits>
+#include <random>
+#include <chrono>
+
+#include <cassert>
+#include <cmath>
+
+//#include <boost/geometry/algorithms/comparable_distance.hpp>
+//#include <boost/geometry/algorithms/intersects.hpp>
 
 #include "basictypes.h"
 #include "plan.h"
@@ -12,32 +19,69 @@
 namespace rrtbase
 {
 
+namespace placeholder_functions
+{
+
+Point sample(Map const& rrtmap)
+{
+  return Point();
+}
+
+int nearest(Graph const& graph, Point const& point)
+{
+  return 0;
+}
+
+Point steer(Point const& from, Point const& to, double maxDistance)
+{
+  return Point();
+}
+
+bool obstacleFree(Map const& m, Point const& from, Point const& to)
+{
+  return true;
+}
+
+std::vector<int> near(Graph const& graph, Point const& point, double maxDistance)
+{
+  return std::vector<int>();
+}
+
+}
+
 namespace default_functions
 {
 
-// Since boost's distance isn't working to Points,
-// lets create a pythagoras funtion
-inline double pythagoras(Point a, Point b)
+Point sample(Map const& rrtmap)
 {
-  double x = a.get<0>() - b.get<0>(),
-         y = a.get<1>() - b.get<1>();
-  return (x * x) + (y * y);
+  static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  static std::mt19937 generator(seed);
+
+  //double x = rrtmap.getSize().get<0>() * generator() / generator.max();
+  //double y = rrtmap.getSize().get<1>() * generator() / generator.max();
+  double x = rrtmap.getSize().get(0) * generator() / generator.max();
+  double y = rrtmap.getSize().get(1) * generator() / generator.max();
+
+  return Point(x, y);
 }
 
-int nearest(const Graph& graph, Point point)
+int nearest(Graph const& graph, Point const& point)
 {
   assert(graph.getVertices().size() > 0);
 
-  int nearest;
-  double minDistance = boost::numeric::bounds<double>::highest();
+  //int nearest;
+  //double minDistance = std::numeric_limits<double>::max();
 
-  Graph::Vertices vertices = graph.getVertices();
+  std::vector<Graph::Vertice> vertices = graph.getVertices();
+
+  int nearest = 0;
+  double minDistance = sqrPythagoras(point, vertices[0].point());
+
   int numVerts = vertices.size();
-  for (int i = 0; i < numVerts; ++i)
+  for (int i = 1; i < numVerts; ++i)
   {
-    // XXX: This should fucking work!!!
     //double dist = boost::geometry::comparable_distance(vertices[nearest].point, vertices[i].point);
-    double dist = pythagoras(point, vertices[i].point);
+    double dist = sqrPythagoras(point, vertices[i].point());
 
     if (dist < minDistance)
     {
@@ -49,9 +93,9 @@ int nearest(const Graph& graph, Point point)
   return nearest;
 }
 
-Point steer(Point from, Point to, double maxDistance)
+Point steer(Point const& from, Point const& to, double maxDistance)
 {
-  double dist = pythagoras(from, to);
+  double dist = sqrPythagoras(from, to);
   if (dist <= maxDistance)
   {
     return to;
@@ -59,48 +103,94 @@ Point steer(Point from, Point to, double maxDistance)
   else
   {
     double t = maxDistance / dist;
-    double dx = to.get<0>() - from.get<0>(),
-           dy = to.get<1>() - from.get<1>();
-    return Point(from.get<0>() + dx * t, from.get<1>() + dy * t);
+    //double dx = to.get<0>() - from.get<0>(),
+    //       dy = to.get<1>() - from.get<1>();
+    double dx = to.get(0) - from.get(0),
+           dy = to.get(1) - from.get(1);
+    //return Point(from.get<0>() + dx * t, from.get<1>() + dy * t);
+    return Point(from.get(0) + dx * t, from.get(1) + dy * t);
   }
 }
 
-bool obstacleFree(const Map& m, Point from, Point to)
+bool obstacleFree(Map const& m, Point const& from, Point const& to)
 {
-  Polygon seg;
-  seg.outer().push_back(from);
-  seg.outer().push_back(to);
+  Segment seg(from, to);
 
-  std::vector<Polygon> polys = m.getPolygons();
-  for (int i = 0; i < polys.size(); ++i)
+  std::vector<Polygon> polygons = m.getPolygons();
+  for (int i = 0; i < polygons.size(); ++i)
   {
-    if (boost::geometry::intersects(seg, polys[i]))
-      return true;
+    //if (boost::geometry::intersects(seg, polys[i]))
+    //  return true;
+
+    if (intersects(seg, polygons[i]))
+      return false;
   }
 
-  return false;
+  return true;
 }
 
-std::vector<int> near(const Graph& graph, Point point)
+std::vector<int> near(Graph const& graph, Point const& point, double maxDistance)
 {
+  std::vector<int> ret;
+
+  std::vector<Graph::Vertice> vertices = graph.getVertices();
+  for (int i = 0; i < vertices.size(); ++i)
+  {
+    //double sqrDist = boost::geometry::comparable_distance(point, vertices[i].point);
+    double sqrDist = sqrPythagoras(point, vertices[i].point());
+    if (sqrDist <= maxDistance * maxDistance)
+      ret.push_back(i);
+  }
+
+  return ret;
 }
 
 }
 
 // Base for RRT, RRG and RRTstar
-Graph plan(Map m,
-          float maxTime,
-          Point (*sample)(),
-          void (*extend)(Point dest,
+void plan(Map const& rrtmap,
+          double maxTime,
+          Graph& graph,
+          void (*extend)(Point const& dest,
+                         Map const& rrtmap,
                          Graph& graph,
-                         int (*nearest)(const Graph&, Point),
-                         int (*steer)(Point, Point),
-                         bool (*obstacleFree)(Point, Point),
-                         std::vector<int> (*near)(const Graph&, Point)))
+                         int (*nearest)(const Graph&, Point const&),
+                         Point (*steer)(Point const&, Point const&, double),
+                         bool (*obstacleFree)(const Map&, Point const&, Point const&),
+                         std::vector<int> (*near)(const Graph&, Point const&, double)),
+          bool anytime,
+          Point (*sample)(Map const& rrtmap),
+          int (*nearest)(const Graph&, Point const&),
+          Point (*steer)(Point const&, Point const&, double),
+          bool (*obstacleFree)(const Map&, Point const&, Point const&),
+          std::vector<int> (*near)(const Graph&, Point const&, double))
 {
-  Graph graph;
+  std::cout << "Initiating planning!" << std::endl;
 
-  return graph;
+  graph.reset();
+  graph.addVertice(rrtmap.getInitialPoint());
+
+  using namespace std::chrono;
+
+  high_resolution_clock::time_point initTime = high_resolution_clock::now();
+  high_resolution_clock::time_point curTime = initTime;
+  milliseconds timeSpan = duration_cast<milliseconds>(curTime - initTime);
+
+  int i = 0;
+  while (timeSpan.count() < maxTime && (anytime || !graph.hasCompleted()))
+  {
+    Point randomPoint = sample(rrtmap);
+    extend(randomPoint, rrtmap, graph, nearest, steer, obstacleFree, near);
+
+    curTime = high_resolution_clock::now();
+    timeSpan = duration_cast<milliseconds>(curTime - initTime);
+    //std::cout << "Current time span: " << timeSpan.count() << " ms." << std::endl;
+
+    i++;
+  }
+
+  std::cout << "Iterations: " << i << std::endl;
+  std::cout << "Completed: " << graph.hasCompleted() << std::endl;
 }
 
 }
--- pathfinding/sampling/RRT/plan.h
+++ pathfinding/sampling/RRT/plan.h
@@ -5,7 +5,8 @@
 
 class A;
 typedef A Edge;
-typedef A Point;
+//typedef A Point;
+class Point;
 
 class Map;
 class Graph;
@@ -13,26 +14,45 @@ class Graph;
 namespace rrtbase
 {
 
+namespace placeholder_functions
+{
+
+Point sample(Map const& rrtmap);
+int nearest(Graph const& graph, Point const& point);
+Point steer(Point const& from, Point const& to, double maxDistance);
+bool obstacleFree(Map const& m, Point const& from, Point const& to);
+std::vector<int> near(Graph const& graph, Point const& point, double maxDistance);
+
+}
+
 namespace default_functions
 {
 
-int nearest(const Graph& graph, Point point);
-Point steer(Point from, Point to, double maxDistance);
-bool obstacleFree(const Map& m, Point from, Point to);
-std::vector<int> near(const Graph& graph, Point point);
+Point sample(Map const& rrtmap);
+int nearest(Graph const& graph, Point const& point);
+Point steer(Point const& from, Point const& to, double maxDistance);
+bool obstacleFree(Map const& m, Point const& from, Point const& to);
+std::vector<int> near(Graph const& graph, Point const& point, double maxDistance);
 
 }
 
 // Base for RRT, RRG and RRTstar
-Graph plan(const Map& m,
-          float maxTime,
-          Point (*sample)(),
-          void (*extend)(Point dest,
+void plan(Map const& rrtmap,
+          double maxTime,
+          Graph& graph,
+          void (*extend)(Point const& to,
+                         Map const& rrtmap,
                          Graph& graph,
-                         int (*nearest)(const Graph&, Point),
-                         Point (*steer)(Point, Point, double),
-                         bool (*obstacleFree)(const Map&, Point, Point),
-                         std::vector<int> (*near)(const Graph&, Point)));
+                         int (*nearest)(Graph const&, Point const&),
+                         Point (*steer)(Point const&, Point const&, double),
+                         bool (*obstacleFree)(Map const&, Point const&, Point const&),
+                         std::vector<int> (*near)(Graph const&, Point const&, double)),
+          bool anytime = false,
+          Point (*sample)(Map const& rrtmap) = default_functions::sample,
+          int (*nearest)(Graph const&, Point const&) = default_functions::nearest,
+          Point (*steer)(Point const&, Point const&, double) = default_functions::steer,
+          bool (*obstacleFree)(Map const&, Point const&, Point const&) = default_functions::obstacleFree,
+          std::vector<int> (*near)(const Graph&, Point const&, double) = default_functions::near);
 
 }
 
