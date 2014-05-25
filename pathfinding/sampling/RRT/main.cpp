8fbb4f00c4424cbc9a7b2753d94a4b73c2a7e6e5
Naum Azeredo <naumazeredo@gmail.com>
2014-05-25 17:45:42 -0300

    Main function

--- pathfinding/sampling/RRT/main.cpp
+++ pathfinding/sampling/RRT/main.cpp
@@ -1,10 +1,23 @@
+#include <iostream>
+
+#include "basictypes.h"
+#include "rrt.h"
+#include "rrg.h"
+#include "plan.h"
+#include "graph.h"
 #include "map.h"
 
 int main()
 {
-  Map theMap;
+  Map rrtmap;
+  rrtmap.setSize(Point(10, 10));
+  rrtmap.setInitialPoint({0.5, 0.5});
+  rrtmap.setGoalRect({{9.5, 9.5}, {10, 10}});
 
-  theMap.loadMap("mapname");
+  rrtbase::Graph rrtgraph;
+
+  rrtbase::plan(rrtmap, 16.66, rrtgraph, rrt::extend);
+  rrtbase::plan(rrtmap, 1000.0 / 60.0, rrtgraph, rrg::extend);
 
   return 0;
 }
