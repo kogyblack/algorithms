98411e3e3cd74a3d5056898137051a38eda8315b
Naum Azeredo <naumazeredo@gmail.com>
2014-05-25 17:42:13 -0300

    Graph structure for RRT algorithms

--- pathfinding/sampling/RRT/graph.cpp
+++ pathfinding/sampling/RRT/graph.cpp
@@ -1,19 +1,155 @@
+#include <algorithm>
 #include <cassert>
 
 #include "graph.h"
+#include "basictypes.h"
 
 namespace rrtbase
 {
 
-// Setters
-void Graph::addVertice(Vertice vertice)
+// Vertice
+//Graph::Vertice::Vertice() : point_(), parent_(nullptr), cost_(0.0)
+Graph::Vertice::Vertice() : point_(), parent_(-1), cost_(0.0)
+{}
+
+Graph::Vertice::Vertice(Point point)
+    //: point_(point), parent_(nullptr), cost_(0.0)
+    : point_(point), parent_(-1), cost_(0.0)
+{}
+
+Point& Graph::Vertice::point()
 {
-  vertices_.push_back(vertice);
+  return point_;
 }
 
-void Graph::addEdge(int v1, int v2)
+/*
+Graph::Vertice* Graph::Vertice::parent()
 {
-  edges_.push_back(Edge(v1, v2));
+  return parent_;
+}
+*/
+
+Point const& Graph::Vertice::point() const
+{
+  return point_;
+}
+
+double const& Graph::Vertice::cost() const
+{
+  return cost_;
+}
+
+void Graph::Vertice::addAncestor(int vertice)
+{
+  if (find(ancestors_.begin(), ancestors_.end(), vertice) == ancestors_.end())
+    return;
+
+  ancestors_.push_back(vertice);
+}
+
+void Graph::Vertice::removeAncestor(int vertice)
+{
+  auto ancestor = find(ancestors_.begin(), ancestors_.end(), vertice);
+  if (ancestor != ancestors_.end())
+    ancestors_.erase(ancestor);
+}
+
+void Graph::Vertice::addDescendant(int vertice)
+{
+  if (find(descendants_.begin(), descendants_.end(), vertice) == descendants_.end())
+    return;
+
+  descendants_.push_back(vertice);
+}
+
+void Graph::Vertice::removeDescendant(int vertice)
+{
+  auto descendant = find(descendants_.begin(), descendants_.end(), vertice);
+  if (descendant != descendants_.end())
+    descendants_.erase(descendant);
+}
+
+/*
+void Graph::Vertice::updateCost(std::vector<Vertice>& vertices)
+{
+  if (ancestors_.size() <= 0)
+    return;
+
+  if (vertices.size() <= 0)
+    return;
+
+  int minAncestor = 0;
+  double minCost = calculateCost(0, vertices);
+  for (int i = 1; i < ancestors_.size(); ++i)
+  {
+    double newCost = calculateCost(i, vertices);
+    if (newCost < minCost)
+    {
+      minAncestor = i;
+      minCost = newCost;
+    }
+  }
+
+  parent_ = &vertices[ancestors_[minAncestor]];
+  cost_ = minCost;
+}
+*/
+
+std::vector<int> const& Graph::Vertice::getAncestors() const
+{
+  return ancestors_;
+}
+
+std::vector<int> const& Graph::Vertice::getDescendants() const
+{
+  return descendants_;
+}
+
+/*
+double Graph::Vertice::calculateCost(int ancestor, std::vector<Vertice> const& vertices) const
+{
+  Graph::Vertice vert = vertices[ancestors_[ancestor]];
+  return vert.cost() + sqrPythagoras(vert.point(), point_);
+}
+*/
+
+
+// Graph
+// Setters
+int Graph::addVertice(Point point)
+{
+  Vertice newVertice(point);
+  vertices_.push_back(newVertice);
+
+  return vertices_.size() - 1;
+}
+
+void Graph::addEdge(int parent, int child)
+{
+  assert(parent >= 0 && parent < vertices_.size());
+  assert(child >= 0 && child < vertices_.size());
+
+  if (find(edges_.begin(), edges_.end(), Edge(parent, child)) == edges_.end())
+    return;
+
+  edges_.push_back(Edge(parent, child));
+  vertices_[parent].addDescendant(child);
+  vertices_[child].addAncestor(parent);
+  //vertices_[child].updateCost(vertices_);
+}
+
+void Graph::removeEdge(int parent, int child)
+{
+  assert(parent >= 0 && parent < vertices_.size());
+  assert(child >= 0 && child < vertices_.size());
+
+  auto edge = find(edges_.begin(), edges_.end(), Edge(parent, child));
+  if (edge != edges_.end())
+    edges_.erase(edge);
+
+  vertices_[parent].removeDescendant(child);
+  vertices_[child].removeAncestor(parent);
+  //vertices_[child].updateCost(vertices_);
 }
 
 void Graph::setCompleted(bool completed)
@@ -21,13 +157,51 @@ void Graph::setCompleted(bool completed)
   completed_ = completed;
 }
 
+void Graph::reset()
+{
+  vertices_.clear();
+  edges_.clear();
+  completed_ = false;
+}
+
+void Graph::updateCost(int vertice)
+{
+  if (vertices_.size() <= 0)
+    return;
+
+  Vertice* vert = &vertices_[vertice];
+  std::vector<int> ancestors = vert->ancestors_;
+  if (ancestors.size() <= 0)
+    return;
+
+  int minAncestor = 0;
+  //double minCost = calculateCost(0, vertices);
+  double minCost = simulateCost(vertice,
+                                ancestors[0]);
+  for (int i = 1; i < vert->ancestors_.size(); ++i)
+  {
+    //double newCost = calculateCost(i, vertices);
+    double newCost = simulateCost(vertice,
+                                  ancestors[i]);
+    if (newCost < minCost)
+    {
+      minAncestor = i;
+      minCost = newCost;
+    }
+  }
+
+  //vertices_[vertice].parent_ = &vertices_[ancestors[minAncestor]];
+  vertices_[vertice].parent_ = ancestors[minAncestor];
+  vertices_[vertice].cost_ = minCost;
+}
+
 // Getters
-std::vector<Graph::Vertice> Graph::getVertices() const
+std::vector<Graph::Vertice> const& Graph::getVertices() const
 {
   return vertices_;
 }
 
-std::vector<Graph::Edge> Graph::getEdges() const
+std::vector<Graph::Edge> const& Graph::getEdges() const
 {
   return edges_;
 }
@@ -40,6 +214,16 @@ Graph::Vertice* Graph::getMinimum() const
 bool Graph::hasCompleted() const
 {
   return completed_;
+}
+
+double const& Graph::getCost(int vertice) const
+{
+  return vertices_[vertice].cost_;
+}
+
+double Graph::simulateCost(int from, int to) const
+{
+  return sqrPythagoras(vertices_[from].point_, vertices_[to].point_);
 }
 
 }
--- pathfinding/sampling/RRT/graph.h
+++ pathfinding/sampling/RRT/graph.h
@@ -6,35 +6,68 @@
 namespace rrtbase
 {
 
+class Graph;
+
 class Graph
 {
 public:
-  // Structures
-  struct Vertice
+  class Vertice;
+  class Vertice
   {
-    Point point;
-    float cost;
+    friend class Graph;
+  public:
+    Vertice();
+    Vertice(Point);
+
+    Point& point();
+    //Vertice* parent();
+
+    Point const& point() const;
+    double const& cost() const;
+  private:
+    void addAncestor(int);
+    void removeAncestor(int);
+    void addDescendant(int);
+    void removeDescendant(int);
+    //void updateCost(std::vector<Vertice>&);
+
+    std::vector<int> const& getAncestors() const;
+    std::vector<int> const& getDescendants() const;
+    //double calculateCost(int, std::vector<Vertice> const&) const;
+
+    Point point_;
+    //Vertice* parent_;
+    int parent_;
+    std::vector<int> descendants_;
+    std::vector<int> ancestors_;
+    double cost_;
   };
 
   // Typedef
   typedef std::pair<int, int>  Edge;
-  typedef std::vector<Vertice> Vertices;
-  typedef std::vector<Edge>    Edges;
 
   // Setters
-  void addVertice(Vertice vertice);
-  void addEdge(int v1, int v2);
+  int addVertice(Point point);
+  //void removeVertice(int vertice);
+  void addEdge(int parent, int child);
+  void removeEdge(int parent, int child);
   void setCompleted(bool completed);
+  void reset();
+
+  void updateCost(int vertice);
 
   // Getters
-  std::vector<Vertice> getVertices() const;
-  std::vector<Edge> getEdges() const;
+  std::vector<Vertice> const& getVertices() const;
+  std::vector<Edge> const& getEdges() const;
   Vertice* getMinimum() const;
   bool hasCompleted() const;
 
+  double const& getCost(int vertice) const;
+  double simulateCost(int from, int to) const;
+
 private:
-  Vertices vertices_;
-  Edges edges_;
+  std::vector<Vertice> vertices_;
+  std::vector<Edge> edges_;
   Vertice* minimum_;
   bool completed_;
 };
