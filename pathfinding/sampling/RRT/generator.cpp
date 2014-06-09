023067186f9cd6cf54497c52acc118c019650ff5
Naum Azeredo <naumazeredo@gmail.com>
2014-06-09 19:41:52 -0300

    Added a random seed to mersenne twister generator

--- pathfinding/sampling/RRT/generator.cpp
+++ pathfinding/sampling/RRT/generator.cpp
@@ -5,7 +5,9 @@
 
 double generator()
 {
-  static std::mt19937 mersenne;
+  // TODO: receive seed to allow reproducing the same tree when needed
+  static unsigned seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  static std::mt19937 mersenne {seed};
   static std::uniform_real_distribution<double> distribution {0.0, 1.0};
 
   return distribution(mersenne);
