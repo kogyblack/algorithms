aad3eeb02d9dcbce2cd82aaf6466a802901070c4
Naum Azeredo <naumazeredo@gmail.com>
2016-05-12 09:22:05 -0300

    Commiting before backup

--- DP/lis.cpp
+++ DP/lis.cpp
@@ -10,20 +10,13 @@ using namespace std;
 
 int lis(int vec[], int n)
 {
-  vector<int> l;
-  l.reserve(n);
-  for (int i = 0; i < n; ++i)
-    l.push_back(1);
+  vector<int> l (n, 1);
 
+  int maxi = 1;
   for (int i = 1; i < n; ++i)
     for (int j = 0; j < i; ++j)
-      if (vec[i] > vec[j] && l[j] + 1 > l[i])
-        l[i] = l[j] + 1;
-
-  int maxi = 0;
-  for (int i = 0; i < n; ++i)
-    if (l[i] > maxi)
-      maxi = l[i];
+      if (vec[i] > vec[j])
+        l[i] = max(l[i], 1+l[j]), maxi = max(maxi, l[i]);
 
   return maxi;
 }
